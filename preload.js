f08536815813c9a6c133899133979a99b579d1cd
Reto Gmür <reto@factsmission.com>
2021-04-13 17:29:40 +0200

    upgraded to the latest node version without context isolation

--- preload.js
+++ preload.js
@@ -1,10 +1,12 @@
 const ssb = require('ssb-client')
+//require('patchboot/public/patch-boot.js')
+    
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
   console.log('loaded')
-  const patchBoot = window.patchBoot
+  const patchBoot = document.createElement('patch-boot')
   patchBoot.ssbConnect = () => new Promise((resolve, reject) => {
     ssb((err, sbot) => {
       if (err) {
