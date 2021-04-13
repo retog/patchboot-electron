31fa4729025d4934544e39362f125ce7b48e6d00
Reto Gmür <reto@factsmission.com>
2021-04-13 17:43:07 +0200

    upgraded to electron 12 with context isolation

--- preload.js
+++ preload.js
@@ -1,20 +1,12 @@
 const ssb = require('ssb-client')
-//require('patchboot/public/patch-boot.js')
-    
+const { contextBridge } = require('electron')
 
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  console.log('loaded')
-  const patchBoot = document.createElement('patch-boot')
-  patchBoot.ssbConnect = () => new Promise((resolve, reject) => {
-    ssb((err, sbot) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(sbot)
-      }
-    });
-  })
-  document.body.appendChild(patchBoot)
-})
+contextBridge.exposeInMainWorld('localSsbConnect', () => new Promise((resolve, reject) => {
+  ssb((err, sbot) => {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(sbot)
+    }
+  });
+}))
